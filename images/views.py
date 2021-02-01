753fc6919767c69bf8fb2bc0d9502de10caad334
Paweł Florczak <pawel.florczak87@gmail.com>
2021-02-01 18:40:39 +0100

    2021.02.01

--- images/views.py
+++ images/views.py
@@ -1,3 +1,5 @@
+import redis
+from django.conf import settings
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
@@ -9,6 +11,7 @@ from .models import Image
 from actions.utils import create_action
 from common.decorators import ajax_required
 
+r = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
 
 @login_required
 def image_create(request):
@@ -30,12 +33,15 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    total_views = r.incr('image:{}:views'.format(image.id))
+    r.zincrby('image_ranking', 1, image.id)
     return render(
         request,
         'images/image/detail.html',
         {
             'section': 'images',
-            'image': image
+            'image': image,
+            'total_views': total_views
         }
     )
 
@@ -89,3 +95,14 @@ def image_list(request):
             'images': images
         }
     )
+
+@login_required
+def image_ranking(request):
+    image_ranking = r.zrange('image_ranking', 0, -1, desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+    most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(request, 'images/image/ranking.html', {
+        'section': 'images',
+        'most_viewed': most_viewed
+    })
